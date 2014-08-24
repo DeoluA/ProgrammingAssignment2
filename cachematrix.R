c5904e5457918cf260c736badbffecafe8c186c7
DeoluA <xltdnc@yahoo.com>
2014-08-24 20:41:11 +0100

    Update cachematrix.R
    My Programming Assignment 2 For Coursera's R Programming Course

--- cachematrix.R
+++ cachematrix.R
@@ -7,32 +7,57 @@
 ## Usage format: special_matrix <- makeCacheMatrix(normal_matrix)
 
 makeCacheMatrix <- function(x = matrix()) {
+  ## 'm' will be used to store the inverse
   m <- NULL
+  
+  ## 'x$set' function only assigns the value of the original matrix
   set <- function(y) {
     x <<- y
     m <<- NULL
   }
-  get <- function() x
-  setinverse <- function(solve) m <<- solve
-  getinverse <- function() m
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse) 
+  
+  ## 'x$get' function only retrieves the value of the original matrix
+  get <- function() {
+    x }
+  
+  ## 'x$setinverse' is the assignment function for the inverse of the 
+  ## | original matrix
+  setinverse <- function(solve) {
+    m <<- solve }
+  
+  ## 'x$getinverse' function only retrieves the inverse of the original matrix
+  getinverse <- function() {
+    m }
+  
+  ## The returned list contains the parameters of our special 'matrix'
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse) 
 }
+
 ## Function 'cacheSolve' calculates, caches and returns the (cached) inverse
 ## |of the returned 'matrix' of  makeCacheMatrix, as well as indicating if that
 ## |value has been previously calculated
 ## Usage format: cacheSolve(special_matrix)
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of matrix 'x'
+  
+  ## Retrieve inverse and assign to m
   m <- x$getinverse()
+  
+  ## Check if m has a value (i.e. if the inverse has been calculated before),
+  ## |indicate if so
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
+  
+  ## Else, calculate inverse
   data <- x$get()
   m <- solve(data, ...)
+  
+  ## Store inverse in special_matrix 'setinverse' column
   x$setinverse(m)
+  
+  ## Return m
   m
 }
